a3e7f408c6ec384902268ce178292f032cc6aad0
Emin Furkan Eren <141373138+eeminfurkan@users.noreply.github.com>
2025-07-03 14:49:11 +0300

    Revert "Add transaction validation to category deletion"
    This reverts commit 5dad30c0912c29d0a312343699d4db52b5a2b89d.

--- FinansAsistan.Api/Features/Categories/Commands/DeleteCategoryCommandHandler.cs
+++ FinansAsistan.Api/Features/Categories/Commands/DeleteCategoryCommandHandler.cs
@@ -1,5 +1,4 @@
-﻿using FinansAsistan.Api.Exceptions; // ValidationException için
-using FinansAsistan.Core.Interfaces;
+﻿using FinansAsistan.Core.Interfaces;
 using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,26 +8,14 @@ namespace FinansAsistan.Api.Features.Categories.Commands
     public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand>
     {
         private readonly ICategoryRepository _categoryRepository;
-        private readonly ITransactionRepository _transactionRepository; // <-- YENİ
 
-        public DeleteCategoryCommandHandler(ICategoryRepository categoryRepository, ITransactionRepository transactionRepository) // <-- GÜNCELLENDİ
+        public DeleteCategoryCommandHandler(ICategoryRepository categoryRepository)
         {
             _categoryRepository = categoryRepository;
-            _transactionRepository = transactionRepository; // <-- YENİ
         }
 
         public async Task Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
         {
-            // ---- YENİ EKLENEN İŞ KURALI KONTROLÜ ----
-            var hasTransactions = await _transactionRepository.HasTransactionsWithCategoryAsync(request.Id);
-            if (hasTransactions)
-            {
-                // Eğer bu kategoriye ait işlemler varsa, kendi ValidationException'ımızı fırlatıyoruz.
-                // Middleware'imiz bunu yakalayıp 400 Bad Request'e çevirecek.
-                throw new ValidationException("Bu kategoriye atanmış işlemler bulunmaktadır. Silmeden önce işlemleri başka bir kategoriye taşıyın.");
-            }
-            // ---- KONTROL SONU ----
-
             await _categoryRepository.DeleteAsync(request.Id);
         }
     }
